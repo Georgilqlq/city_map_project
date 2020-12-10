fc7feb1f4a5351619c81c364ade1c2015d8eb0a7
Georgilqlq <lqlqgeorgi@abv.bg>
2020-12-10 18:37:25 +0200

    added two more functions again

--- project/graph.cpp
+++ project/graph.cpp
@@ -104,6 +104,60 @@ void Graph::find_all_paths(const std::string &from_vertex, const std::string &to
     }
 }
 
+void Graph::find_all_paths(const std::string &from_vertex, const std::string &to_vertex, const std::set<std::string> &closed_vertices)
+{
+    if (is_it_reachable(from_vertex, to_vertex) == false)
+    {
+        return;
+    }
+
+    std::list<std::vector<std::string>> paths;
+    std::map<std::string, bool> visited_vertices;
+    std::vector<std::string> current_path;
+
+    for (auto vertex : vertices)
+    {
+        if (closed_vertices.count(vertex.first) == 1)
+        {
+            visited_vertices[vertex.first] = true;
+        }
+        else
+        {
+            visited_vertices[vertex.first] = false;
+        }
+    }
+
+    find_all_paths_helper(from_vertex, to_vertex, visited_vertices, paths, current_path);
+
+    for (size_t i = 0; i < 3 && !paths.empty(); i++)
+    {
+        unsigned int min_weight = UINT_MAX;
+        std::vector<std::string> min_path;
+        for (auto vector : paths)
+        {
+            unsigned int total_weight = 0;
+            std::vector<std::string>::const_iterator end = vector.cend() - 1;
+            for (std::vector<std::string>::const_iterator element = vector.cbegin(); element != end; ++element)
+            {
+                total_weight = total_weight + edge_weigh[std::make_pair(*element, *(element + 1))];
+            }
+
+            if (total_weight < min_weight)
+            {
+                min_weight = total_weight;
+                min_path = vector;
+            }
+        }
+
+        for (auto element : min_path)
+        {
+            std::cout << element << " ";
+        }
+        std::cout << min_weight << std::endl;
+        paths.remove(min_path);
+    }
+}
+
 void Graph::find_all_paths_helper(const std::string &from_vertex, const std::string &to_vertex, std::map<std::string, bool> &visited_vertices, std::list<std::vector<std::string>> &paths, std::vector<std::string> &current_path)
 {
     visited_vertices[from_vertex] = true;
@@ -156,4 +210,41 @@ void Graph::find_all_dead_ends()
             }
         }
     }
+}
+
+bool Graph::mini_tour_and_return(const std::string &from_vertex)
+{
+    std::map<std::string, bool> visited_vertices;
+    std::stack<std::string> path;
+
+    for (auto vertex : vertices)
+    {
+        visited_vertices[vertex.first] = false;
+    }
+
+    // visited_vertices[from_vertex] = true;
+    path.push(from_vertex);
+
+    std::string current_vertex;
+    while (!path.empty())
+    {
+        current_vertex = path.top();
+        path.pop();
+
+        for (auto neighbour : vertices[current_vertex])
+        {
+            if (neighbour == from_vertex)
+            {
+                return true;
+            }
+
+            if (visited_vertices[neighbour] == false)
+            {
+                visited_vertices[neighbour] = true;
+                path.push(neighbour);
+            }
+        }
+    }
+
+    return false;
 }
--- project/main.cpp
+++ project/main.cpp
@@ -15,9 +15,15 @@ int main()
     test_graph.add_edge("3", "1", 4);
     test_graph.add_edge("3", "2", 5);
     test_graph.add_edge("2", "4", 6);
+    test_graph.add_edge("4", "3", 10);
 
     std::cout << test_graph.is_it_reachable("3", "4") << std::endl;
     test_graph.find_all_paths("3", "4");
     std::cout << test_graph.can_reach_all_vertices("4") << std::endl;
     test_graph.find_all_dead_ends();
+
+    std::set<std::string> close_test;
+    close_test.insert("1");
+    test_graph.find_all_paths("3", "4", close_test);
+    std::cout << test_graph.mini_tour_and_return("2");
 }
